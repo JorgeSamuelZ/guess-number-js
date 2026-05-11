39f9765e3867697f7ef0bc2e4c1a9ce1d6b80505
Jorge Zorrilla <zorrillajorgesamuel@gmail>
2026-05-11 15:56:15 -0500

    feat: Avance 1

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log('¡Hola me llamo Jorge!');
+
+
+let nombrePropio = "Samuel";
+
+console.log(nombrePropio);
+
+nombrePropio = "Jorge";
+
+console.log(nombrePropio);
+
+console.log(typeof nombrePropio);
+
+let apellidoPaterno = "Zorrilla";
+
+const CURSO = "Code 101";
+
+console.log(CURSO);
+
+let miVariable = true;
+let miVariable2 = false;
+
+console.log(miVariable);
+
+
+
+let edadUsuario = prompt('¿Cúantos años tienes?');
+
+console.log(edadUsuario);
+console.log(typeof edadUsuario);
+
+edadUsuario = Number(edadUsuario)
+
+console.log(typeof edadUsuario);
+
+console.log(edadUsuario+5);
+
+let saludoModerno = `Me llamo ${nombrePropio} ${apellidoPaterno} y estoy en el curso ${CURSO}`;
+
+console.log(saludoModerno);
